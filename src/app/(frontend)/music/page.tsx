a3d60053e60994c734af426c723ef54d4e953d06
Travis Ehrenstrom <tehrenstrom@gmail.com>
2025-12-30 20:45:44 -0800

    fix: Remove non-existent id property from link key

--- src/app/(frontend)/music/page.tsx
+++ src/app/(frontend)/music/page.tsx
@@ -218,7 +218,7 @@ export default async function MusicPage() {
                     <a
                       className={buttonVariants({ size: 'sm', variant: 'outline' })}
                       href={link.url}
-                      key={link.id ?? link.url}
+                      key={link.url}
                       rel="noreferrer"
                       target="_blank"
                     >
